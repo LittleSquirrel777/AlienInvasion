8fc8f99cf7ef780ef72f2435481297111df92cd2
LittleSquirrel777 <2436700441@qq.com>
2022-09-18 17:36:52 +0800

    添加子弹类，能够空格发射子弹，每次只能发送一颗，如果按住空格不断发送会导致子弹重合

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -17,6 +18,9 @@ class AlienInvasion:
         self.ship = Ship(self)
         self.settings.screen_width = self.screen.get_width()
         self.settings.screen_height = self.screen.get_height()
+        # 创建存储子弹的编组
+        self.bullets = pygame.sprite.Group()
+
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -24,6 +28,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullet()
             self._update_screen()
 
     def _check_events(self):
@@ -42,7 +47,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
-        elif event.key == pygame.K_q:
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
             sys.exit()
 
     def _check_keyup_events(self, event):
@@ -52,10 +59,26 @@ class AlienInvasion:
         elif event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
 
+    def _fire_bullet(self):
+        """发射子弹添加到编组中"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+    def _update_bullet(self):
+        """更新所有子弹的位置，删除超过屏幕上界的子弹"""
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         """更新屏幕图像，并切换到到新屏幕"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        """绘制每一个子弹"""
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 
