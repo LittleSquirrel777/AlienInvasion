d760656522f609b0a6faf4d2927ecbf372c17234
LittleSquirrel777 <2436700441@qq.com>
2022-09-18 15:56:20 +0800

    注册事件，可以左右移动飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,22 +11,49 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # 这里修改游戏屏幕大小为整个屏幕
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.ship = Ship(self)
+        self.settings.screen_width = self.screen.get_width()
+        self.settings.screen_height = self.screen.get_height()
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
+        """响应按键和鼠标事件"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按键"""
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应松开键盘"""
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
 
     def _update_screen(self):
+        """更新屏幕图像，并切换到到新屏幕"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         pygame.display.flip()
--- ship.py
+++ ship.py
@@ -2,14 +2,37 @@ import pygame
 
 
 class Ship:
+    """管理飞船的类"""
     def __init__(self, ai_game):
+        """初始化飞船，并设置其初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
+        """加载飞船图像并获取其外接矩形"""
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
 
+        """设置飞船的初始位置"""
         self.rect.midbottom = self.screen_rect.midbottom
 
+        """存储飞船的实际位置"""
+        self.x = float(self.rect.x)
+
+        """设置飞船的移动标记"""
+        self.moving_left = False
+        self.moving_right = False
+
+    def update(self):
+        """根据标记移动飞船的实际位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        """重新设置飞船的位置"""
+        self.rect.x = self.x
+
     def blitme(self):
+        """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
